815ab610838bb9438b89fdaf9e47a5ad01ea175f
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-02-09 18:50:03 +0800

    使用supabase service沒有引入檔案導致錯誤

--- app/Http/Controllers/FishController.php
+++ app/Http/Controllers/FishController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateFishRequest;
 use App\Models\Fish;
+use App\Services\SupabaseStorageService;
 
 class FishController extends Controller
 {
