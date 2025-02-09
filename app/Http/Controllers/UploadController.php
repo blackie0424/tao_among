da741e48657e3539fbb96dd3d37402211771fcb3
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-02-09 14:25:30 +0800

    去除舊的上傳到supabase函式

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -40,31 +40,4 @@ class UploadController extends Controller
             return response()->json(['message' => 'image uploaded successfully', 'data' => $imageName], 201);
         }
     }
-
-    public function uploadImageToSupabase()
-    {
-        $request = request();
-        try {
-            $request->validate([
-                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            ]);
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            return response()->json(['message' => 'image upload failed', 'data' => $e->errors()], 400);
-        } catch (Exception $e) {
-            return response()->json(['message' => 'Upload failed', 'error' => $e->getMessage()], 500);
-        }
-
-        $file = $request->file('image');
-        $path = 'images';
-        $storageService = new SupabaseStorageService;
-
-        $filePath = $storageService->uploadFile($file, $path);
-        if (! $filePath) {
-            return response()->json(['message' => 'Upload failed'], 500);
-        }
-        $url = $storageService->getUrl($filePath);
-        $imageName = basename($filePath);
-
-        return response()->json(['message' => 'image uploaded successfully', 'data' => $imageName], 201);
-    }
 }
