91156f00947497d5f0c0bb999eeb86819b891645
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-12 16:13:00 +0800

    refactor:特定魚類的地區區塊修改成component

---
+++ resources/js/Components/FishLocateButtons.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="button-container">
+        <div class="section-buttons flex flex-wrap md:flex-nowrap gap-2 md:gap-4 my-4 p-4 rounded-lg">
+          <a
+            v-for="locate in locates"
+            :key="locate.value"
+            :href="`?locate=${locate.value}`"
+            class="locate-filter block w-full sm:w-1/2 md:w-auto md:min-w-[120px] px-6 py-2 min-h-[48px] flex items-center justify-center bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 rounded-full hover:bg-blue-200 dark:hover:bg-blue-700"
+            :class="{'bg-yellow-500 dark:bg-yellow-600': currentLocate === locate.value}"
+          >
+            {{ locate.label }}
+          </a>
+        </div>
+      </div>
+</template>
+
+<script setup>
+defineProps({
+  locates: Array
+});
+</script>
--- resources/js/Pages/Fish.vue
+++ resources/js/Pages/Fish.vue
@@ -23,19 +23,8 @@
 
 
       <!-- 按鈕區塊 -->
-      <div class="button-container">
-        <div class="section-buttons flex flex-wrap md:flex-nowrap gap-2 md:gap-4 my-4 p-4 rounded-lg">
-          <a
-            v-for="locate in locates"
-            :key="locate.value"
-            :href="`?locate=${locate.value}`"
-            class="locate-filter block w-full sm:w-1/2 md:w-auto md:min-w-[120px] px-6 py-2 min-h-[48px] flex items-center justify-center bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 rounded-full hover:bg-blue-200 dark:hover:bg-blue-700"
-            :class="{'bg-yellow-500 dark:bg-yellow-600': currentLocate === locate.value}"
-          >
-            {{ locate.label }}
-          </a>
-        </div>
-      </div>
+      <FishLocateButtons :locates="locates" />
+
 
       <!-- 筆記區塊 -->
       <div v-if="fish.notes && fish.notes.length" class="w-full flex flex-col items-center">
@@ -49,6 +38,7 @@
         </div>
       </div>
     </div>
+    
     <footer class="text-center text-secondary mt-8">Copyright © 2025 Chungyueh</footer>
   </div>
 </template>
@@ -59,6 +49,7 @@ import { router } from '@inertiajs/vue3';
 
 import FishImage from '@/Components/FishImage.vue';
 import FishName from '@/Components/FishName.vue';
+import FishLocateButtons from '@/Components/FishLocateButtons.vue';
 
 
 
