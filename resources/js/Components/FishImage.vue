8b1062915403f8794b6ffd6578942d4a424f0a3a
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-14 01:34:06 +0800

    bug:解決在手機上，魚類圖片跟背景白色底圖會有上下留白過長的問題。

--- resources/js/Components/FishImage.vue
+++ resources/js/Components/FishImage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="show_image w-full max-w-3xl mx-auto flex items-center justify-center mb-6 p-4 rounded-lg shadow-custom">
-    <img :src="image" :alt="name" loading="lazy" class="max-h-full max-w-full object-contain rounded-lg " />
+  <div class="show_image w-full max-w-3xl mx-auto flex items-center justify-center mb-6 p-4 rounded-lg shadow-custom h-80 md:h-[32rem]">
+    <img :src="image" :alt="name" loading="lazy" class="max-h-full max-w-full object-contain rounded-lg" />
   </div>
 </template>
 <script setup>
