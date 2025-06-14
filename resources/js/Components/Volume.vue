b87b986607a36d7f38800e7e5562f8e6b63f9a45
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-14 12:06:34 +0800

    feat:新增發音按鈕，目前都無法播放，只是示意

---
+++ resources/js/Components/Volume.vue
@@ -0,0 +1,14 @@
+<template>
+    <span class="inline-flex items-center justify-center rounded-full bg-gray-200 ml-2" style="width: 2.2rem; height: 2.2rem;">
+        <img
+          :src="volumeIcon"
+          alt="volume"
+          class="w-5 h-5 opacity-50"
+          style="filter: grayscale(1);"
+        />
+      </span>
+</template>
+
+<script setup>
+    const volumeIcon = '/images/volume.png';
+</script>
