7eb28a47f121f847cba0a2636484b41c0293a307
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-16 15:03:29 +0800

    feat: 新增 factory 與 seeder，支援產生 fish、fish_note、fish_size 假資料供測試

---
+++ database/seeders/FishSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use \App\Models\FishNote;
+use \App\Models\FishSize;
+use \App\Models\Fish;
+
+
+class FishSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Fish::factory()
+            ->count(10)
+            ->has(FishNote::factory()->count(10), 'notes')
+            ->has(FishSize::factory(), 'size')
+            ->create();
+    }
+}
