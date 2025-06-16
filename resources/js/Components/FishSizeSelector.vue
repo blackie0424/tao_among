d269ece26b53f461188dcad0f7d02743acf6e46d
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-16 13:26:52 +0800

    refactor:新增魚類尺寸，下方的文字顯示已經刪除，使用者只需要看圖片呈現的結果即可

--- resources/js/Components/FishSizeSelector.vue
+++ resources/js/Components/FishSizeSelector.vue
@@ -2,7 +2,6 @@
   <div class="bg-white p-6 rounded shadow-md max-w-md mx-auto">
     <h3 class="text-xl font-bold mb-4">選擇魚的尺寸</h3>
     <ArmSelector v-model="selectedParts" :readonly="false" />
-    <pre>{{ selectedParts }}</pre>
     <button class="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 mt-4"
             @click="submitFishSize"
             :disabled="sizeSubmitting">
