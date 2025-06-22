353d4779a611d8b4fe946c45e8362edc474bafe1
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-06-22 10:26:47 +0800

    refactor:修正fish name與overflow name的位置設定。

--- resources/js/Components/FishName.vue
+++ resources/js/Components/FishName.vue
@@ -1,21 +1,14 @@
 <template>
-  <div class="section section-name w-full max-w-3xl p-4 mb-4 flex flex-col items-end relative">
-    <div class="flex items-center justify-center w-full mb-4">
+  <div class="section section-name w-full max-w-3xl p-4 mb-4 flex flex-col items-end">
+    <div class="flex justify-between w-full mb-4">
       <div class="text text-xl text-secondary">ngaran no among</div>
+      <OverflowMenu :apiUrl="`/prefix/api/fish/${fishId}`" :redirectUrl="`/`" />
     </div>
     <!-- 魚名與 icon 水平排列 -->
-    <div
-      class="section-title text-2xl font-bold text-primary flex items-center justify-center w-full"
-    >
+    <div class="section-title text-2xl font-bold text-primary flex justify-between w-full">
       <span>{{ fishName }}</span>
-      <Volume class="ml-4" />
+      <Volume />
     </div>
-
-    <OverflowMenu
-      class="absolute right-0 top-0"
-      :apiUrl="`/prefix/api/fish/${fishId}`"
-      :redirectUrl="`/`"
-    />
   </div>
 </template>
 
