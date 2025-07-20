841705483c0fae73ff17dc92d047272e7b07ab9b
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-07-20 21:14:42 +0800

    feat:修改首頁大圖的位置

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container mx-auto p-4">
-    <main>
+    <div id="index">
       <picture>
         <source media="(min-width: 1025px)" :srcset="headerM" />
         <source media="(min-width: 481px)" :srcset="headerS" />
         <img :src="headerL" class="responsive-img" loading="lazy" />
       </picture>
-      <div class="header-content">
+      <div class="index-content">
         <h1 @click="goFishs" style="cursor: pointer">nivasilan ko a among</h1>
       </div>
-    </main>
+    </div>
     <footer class="mt-8 text-center text-gray-500">Copyright © 2025 Chungyueh</footer>
   </div>
 </template>
