41166b2c09cf47bc5ea410552d9316b1e4a34d91
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-07-21 16:02:29 +0800

    feat:加入安裝app、留懶於累圖件的按扭

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -7,14 +7,25 @@
         <img :src="headerL" class="responsive-img" loading="lazy" />
       </picture>
       <div class="index-content">
-        <h1 @click="goFishs" style="cursor: pointer">nivasilan ko a among</h1>
+        <h1>nivasilan ko a among</h1>
       </div>
+      <button
+        v-if="showInstallBtn"
+        @click="installPWA"
+        class="mt-6 px-4 py-2 bg-green-600 text-white rounded"
+      >
+        安裝 App
+      </button>
+      <button @click="goFishs" class="mt-6 px-4 py-2 bg-green-600 text-white rounded">
+        瀏覽魚類圖鑑
+      </button>
     </div>
     <footer class="mt-8 text-center text-gray-500">Copyright © 2025 Chungyueh</footer>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import { router } from '@inertiajs/vue3'
 const headerM = '/images/header-m.png'
 const headerS = '/images/header-s.png'
@@ -22,5 +33,27 @@ const headerL = '/images/header-l.jpg'
 
 function goFishs() {
   router.visit('/fishs')
+}
+
+// PWA 安裝提示
+const showInstallBtn = ref(false)
+let deferredPrompt = null
+
+onMounted(() => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt = e
+    showInstallBtn.value = true
+  })
+})
+
+function installPWA() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt()
+    deferredPrompt.userChoice.then(() => {
+      showInstallBtn.value = false
+      deferredPrompt = null
+    })
+  }
 }
 </script>
