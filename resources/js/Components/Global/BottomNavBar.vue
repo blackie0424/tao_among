7976a3d693b656529c4bbdc82a8c31210f8f37ef
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-08-03 00:31:39 +0800

    feat:在魚類資訊頁面設定底部控制元件，加入新增知識連結及回到圖鑑頁

--- resources/js/Components/Global/BottomNavBar.vue
+++ resources/js/Components/Global/BottomNavBar.vue
@@ -20,12 +20,9 @@
           ></path>
         </svg>
       </div>
-      <p class="text-[#0e171b] text-xs font-medium leading-normal tracking-[0.015em]">首頁</p>
+      <p class="text-[#0e171b] text-xs font-medium leading-normal tracking-[0.015em]">圖鑑</p>
     </a>
-    <a
-      class="flex flex-1 flex-col items-center justify-end gap-1 text-[#4d7f99]"
-      href="/fish/create"
-    >
+    <a class="flex flex-1 flex-col items-center justify-end gap-1 text-[#4d7f99]" :href="to">
       <div class="text-[#4d7f99] flex h-8 items-center justify-center">
         <!-- Add/New Icon -->
         <svg
@@ -40,7 +37,9 @@
           />
         </svg>
       </div>
-      <p class="text-[#4d7f99] text-xs font-medium leading-normal tracking-[0.015em]">新增</p>
+      <p class="text-[#4d7f99] text-xs font-medium leading-normal tracking-[0.015em]">
+        {{ label }}
+      </p>
     </a>
     <span
       class="flex flex-1 flex-col items-center justify-end gap-1 text-[#b0b9be] cursor-not-allowed opacity-50"
@@ -82,3 +81,11 @@
     </span>
   </nav>
 </template>
+
+<script setup>
+const props = defineProps({
+  label: { type: String, default: '新增知識' },
+  icon: { type: String, default: '+' },
+  to: { type: String, default: '/fishs' },
+})
+</script>
