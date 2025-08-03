c1dc014fde6bf946e4f8a35c82b65ee68e42b800
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-08-04 00:34:56 +0800

    feat:有錄音檔案才顯示volume

--- resources/js/Components/FishName.vue
+++ resources/js/Components/FishName.vue
@@ -7,7 +7,9 @@
     <!-- 魚名與 icon 水平排列 -->
     <div class="section-title text-2xl font-bold text-primary flex justify-between w-full">
       <span>{{ fishName }}</span>
-      <Volume :audioUrl="props.audios.url" />
+      <template v-if="props.audios && props.audios.url">
+        <Volume :audioUrl="props.audios.url" />
+      </template>
     </div>
   </div>
 </template>
