c76d5e9e09ced6f1fe9d4e10cef10fa57186229e
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-09-09 00:21:47 +0800

    feat:修改首頁呈現樣式，移除圖片，改用動畫文字，加快顯示速度

---
+++ resources/js/Components/AnimatedText.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1 :style="{ fontSize: `${fontSize}px` }" class="font-bold flex justify-center">
+    <span
+      v-for="(char, idx) in textArr"
+      :key="idx"
+      :style="{ animationDelay: `${idx * 0.08}s`, marginRight: '0.3em' }"
+      class="animate-outline-fill"
+    >
+      {{ char }}
+    </span>
+  </h1>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  text: String,
+  fontSize: { type: Number, default: 48 },
+})
+const textArr = computed(() => props.text.split(''))
+</script>
+
+<style scoped>
+@keyframes outline-fill {
+  0% {
+    color: transparent;
+    -webkit-text-stroke: 2px #fff;
+    text-stroke: 2px #fff;
+    background: transparent;
+    opacity: 1;
+  }
+  60% {
+    color: transparent;
+    -webkit-text-stroke: 2px #0e171b;
+    text-stroke: 2px #0e171b;
+    background: transparent;
+    opacity: 1;
+  }
+  100% {
+    color: #0e171b;
+    -webkit-text-stroke: 0px #0e171b;
+    text-stroke: 0px #0e171b;
+    background: transparent;
+    opacity: 1;
+  }
+}
+.animate-outline-fill {
+  display: inline-block;
+  opacity: 1;
+  color: transparent;
+  background: transparent;
+  -webkit-text-stroke: 2px #fff; /* 白色簍空 */
+  animation: outline-fill 1s forwards;
+  animation-fill-mode: forwards;
+}
+</style>
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="container mx-auto p-4">
-    <div id="index">
-      <picture>
-        <source media="(min-width: 1025px)" :srcset="headerM" />
-        <source media="(min-width: 481px)" :srcset="headerS" />
-        <img :src="headerL" class="responsive-img" loading="lazy" />
-      </picture>
-      <div class="index-content">
-        <h1>nivasilan ko a among</h1>
-      </div>
+    <div id="index" class="flex flex-col items-center justify-center min-h-[80vh] w-full">
+      <AnimatedText text="nivasilan ko a among" />
       <button
         v-if="showInstallBtn"
         @click="installPWA"
@@ -27,6 +20,8 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { router } from '@inertiajs/vue3'
+import AnimatedText from '@/Components/AnimatedText.vue'
+
 const headerM = '/images/header-m.png'
 const headerS = '/images/header-s.png'
 const headerL = '/images/header-l.jpg'
