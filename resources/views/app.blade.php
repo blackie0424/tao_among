1ebc99422738d4eab4a7331dce81d57a8183a88f
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-10-15 11:45:12 +0800

    feat:更換網頁標題旁邊的圖示

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite(['resources/css/app.css', 'resources/css/fish.css', 'resources/js/app.js'])
     @inertiaHead
+    <link rel="icon" type="image/png" href="{{ asset('icons/icon.png') }}">
     <link rel="manifest" href="/manifest.json">
+
     <meta name="theme-color" content="#1976d2">
     <script src="https://cdn.jsdelivr.net/npm/heic2any/dist/heic2any.min.js"></script>
     <script>
