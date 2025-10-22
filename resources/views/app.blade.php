dbfe62922a0267d96db9d2e4175d8d60f37680fb
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-10-22 21:55:59 +0800

    feat:刪除禁止放大的設定，讓瀏覽器允許縮放

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite(['resources/css/app.css', 'resources/css/fish.css', 'resources/js/app.js'])
     @inertiaHead
     <link rel="icon" type="image/png" href="{{ asset('icons/icon.png') }}">
