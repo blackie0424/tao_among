9fc02a6094b89effb0a89269202f3dc78a3cab4d
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2026-01-01 12:51:29 +0800

    feat:刪除後的動作由overflow menu控制

--- resources/js/Components/FishName.vue
+++ resources/js/Components/FishName.vue
@@ -2,12 +2,7 @@
   <div class="section section-name w-full max-w-3xl p-4 mb-4 flex flex-col items-end">
     <div class="flex justify-between w-full mb-4">
       <div class="text text-xl text-secondary">ngaran no among</div>
-      <OverflowMenu
-        :apiUrl="`/fish/${fishId}`"
-        :redirectUrl="`/fishs`"
-        :fishId="fishId"
-        @deleted="onFishDeleted"
-      />
+      <OverflowMenu :apiUrl="`/fish/${fishId}`" :redirectUrl="`/fishs`" :fishId="fishId" />
     </div>
     <!-- 魚名與 icon 水平排列 -->
     <div class="section-title text-2xl font-bold text-primary flex justify-between w-full">
@@ -29,8 +24,4 @@ const props = defineProps({
   fishId: String,
   audio: String,
 })
-
-function onFishDeleted() {
-  router.visit('/fishs', { preserveScroll: true })
-}
 </script>
