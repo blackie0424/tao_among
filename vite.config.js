6444c9d3bbf78fede918ff6995656630b050dcb6
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2026-07-27 16:55:52 +0800

    fix: use current host for local asset urls
    (cherry picked from commit c59bd33760a768c8ccd374b0f4cbec0fecedfcc2)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/css/fish.css', 'resources/js/app.js'],
